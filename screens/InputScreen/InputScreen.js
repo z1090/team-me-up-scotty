fb24297a7ba6e1024e4c45b616daa10386647ab4
Ian Wildsmith <ian.wildsmith@gmail.com>
2019-04-01 14:12:41 +0100

    Ten inputs saving to local state

--- screens/InputScreen/InputScreen.js
+++ screens/InputScreen/InputScreen.js
@@ -1,22 +1,69 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import { StyleSheet, Text, View, TouchableHighlight, TextInput, ScrollView } from "react-native";
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            names: this.props.names,
+            p1: this.props.names[0],
+            p2: this.props.names[1],
+            p3: this.props.names[2],
+            p4: this.props.names[3],
+            p5: this.props.names[4],
+            p6: this.props.names[5],
+            p7: this.props.names[6],
+            p8: this.props.names[7],
+            p9: this.props.names[8],
+            p10: this.props.names[9],
+        };
+
+
         this.handlePress = this.handlePress.bind(this);
+        // this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handlePress() {
         this.props.navigation.navigate("Teams");
     }
 
+    // handleInputChange(text) {
+    //     this.setState({ names: this.state.names.map((player, i) => (player = i === 0 ? text : player))};
+    // }
+    
+
     render() {
+        console.log(this.state.names);
+
         return (
             <View style={styles.container}>
-                <Text>This is the input screen</Text>
-                <Text>Hello {this.props.names[0]}</Text>
+                <Text>Enter Participants</Text>
+
+                <TextInput style={styles.textInput} value={this.state.names[0]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 0 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[1]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 1 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[2]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 2 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[3]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 3 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[4]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 4 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[5]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 5 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[6]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 6 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[7]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 7 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[8]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 8 ? text : player))})} />
+                <TextInput style={styles.textInput} value={this.state.names[9]}
+                    onChangeText={(text) => this.setState({ names: this.state.names.map((player, i) => (player = i === 9 ? text : player))})} />
+
+
+
+
                 <TouchableHighlight style={styles.btn} underlayColor={"#1B5E20"} onPress={this.handlePress}>
                     <Text style={styles.btnText}>click</Text>
                 </TouchableHighlight>
@@ -31,6 +78,14 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+    },
+    textInput: {
+        borderColor: "#69B569",
+        borderWidth: 1,
+        borderRadius: 5,
+        width: "80%",
+        marginBottom: 8,
+        paddingHorizontal: 5,
     },
     btn: {
         alignItems: "center",
