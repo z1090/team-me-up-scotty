0fd8dd10e95b501af4085f014f7ffb264c9c1c17
Ian Wildsmith <ian.wildsmith@gmail.com>
2019-04-01 18:05:20 +0100

    inputs scroll when covered by keyboard

--- data/initialState.js
+++ data/initialState.js
@@ -1,16 +1,5 @@
 const initialState = {
-    names: [
-        "01 - Jordan Pickford",
-        "02 - Danny Rose",
-        "03 - Michael Keane",
-        "04 - Harry Maguire",
-        "05 - Kyle Walker",
-        "06 - Ross Barkley",
-        "07 - Callum Hudson - Odoi",
-        "08 - Dele Alli",
-        "09 - Raheem Sterling",
-        "10 - Harry Kane",
-    ],
+    names: ["", "", "", "", "", "", "", "", "", ""],
 };
 
 export default initialState;
--- screens/TeamsScreen/TeamsScreen.js
+++ screens/TeamsScreen/TeamsScreen.js
@@ -6,17 +6,17 @@ export default class App extends React.Component {
         return (
             <View style={styles.container}>
                 <Text>Team 1</Text>
-                <Text>{this.props.teams.team1[0]}</Text>
-                <Text>{this.props.teams.team1[2]}</Text>
-                <Text>{this.props.teams.team1[3]}</Text>
-                <Text>{this.props.teams.team1[4]}</Text>
-                <Text>{this.props.teams.team1[5]}</Text>
+                <Text>1 - {this.props.teams.team1[0]}</Text>
+                <Text>2 - {this.props.teams.team1[1]}</Text>
+                <Text>3 - {this.props.teams.team1[2]}</Text>
+                <Text>4 - {this.props.teams.team1[3]}</Text>
+                <Text>5 - {this.props.teams.team1[4]}</Text>
                 <Text>Team 2</Text>
-                <Text>{this.props.teams.team2[0]}</Text>
-                <Text>{this.props.teams.team2[2]}</Text>
-                <Text>{this.props.teams.team2[3]}</Text>
-                <Text>{this.props.teams.team2[4]}</Text>
-                <Text>{this.props.teams.team2[5]}</Text>
+                <Text>1 - {this.props.teams.team2[0]}</Text>
+                <Text>2 - {this.props.teams.team2[1]}</Text>
+                <Text>3 - {this.props.teams.team2[2]}</Text>
+                <Text>4 - {this.props.teams.team2[3]}</Text>
+                <Text>5 - {this.props.teams.team2[4]}</Text>
             </View>
         );
     }
