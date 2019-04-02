71e108ef55b6f13a7fddf359d3dd49dfc6a1768f
Ian Wildsmith <ian.wildsmith@gmail.com>
2019-04-02 22:58:42 +0100

    FlatList on Teams Screen

--- data/initialState.js
+++ data/initialState.js
@@ -5,11 +5,7 @@ let initialState = {
         "03 - Michael Keane",
         "04 - Harry Maguire",
         "05 - Kyle Walker",
-        "06 - Ross Barkley",
-        "07 - Callum Hudson - Odoi",
-        "08 - Dele Alli",
-        "09 - Raheem Sterling",
-        "10 - Harry Kane",
+
     ],
 };
 
